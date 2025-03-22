25915f74126a5732474e85ff6abd8e26715eca50
Gorish Gupta <gorishgupta50@gmail.com>
2025-03-22 19:56:52 +0530

    Create DS2685.java

---
+++ DS2685.java
@@ -0,0 +1,45 @@
+public class DS2685 {
+    class Solution {
+        int[] parent;
+        public int countCompleteComponents(int n, int[][] edges) {
+            parent = new int[n];
+            for(int i =0; i < n; i++){
+                parent[i] = i;
+            }
+            for(int[] edge : edges){
+                int a = edge[0], b = edge[1], pa = parent(a), pb = parent(b);
+                parent[pa] = pb;
+            }
+            int[] nodeCount =  new int[n];
+            int[] edgeCount = new int[n];
+            for(int i = 0; i < n; i++){
+                nodeCount[parent(i)]++;
+            }
+            for(int[] edge : edges){
+                edgeCount[parent(edge[0])]++;
+            }
+            int out = 0;
+            for(int i = 0; i < n; i++){
+                
+                int count = nodeCount[i], expected = (count * (count - 1))/2;
+                if(count == 0){
+                    continue;
+                } else if(expected == edgeCount[i]){
+                    out++;
+                }
+            }
+            return out;
+        }
+        int parent(int n){
+            int p = parent[n];
+            if(p == n){
+                return p;
+            }
+            int realParent = parent(p);
+            parent[n] = realParent;
+            return realParent;
+        }
+    
+        //each component of n nodes needs (n * (n - 1))/2 edges
+    }
+}
